8e25723af7470231ccb9918573833e7602d36b88
Menshuu <jamshidbekuktamov1999@gmail.com>
2025-03-09 16:18:43 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from aiogram import executor
-from bot import dp
-import handlers  # Barcha handlerlar yuklanishi uchun import qilinadi
+from bot import dp  # dp - Dispatcher obyekti
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
