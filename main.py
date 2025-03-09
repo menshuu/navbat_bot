831a95f720f1e4ba3eb0a325e5ed5a67186e0c2a
Menshuu <jamshidbekuktamov1999@gmail.com>
2025-03-09 16:19:01 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from aiogram import executor
-from bot import dp  # dp - Dispatcher obyekti
+import asyncio
+from aiogram import Bot, Dispatcher
+from bot import dp, bot  # `bot.py` ichida `bot` va `dp` bo‘lishi kerak
+
+async def main():
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+    asyncio.run(main())
