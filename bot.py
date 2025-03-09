3d7311ddf1a3a9131fb22efd2c3f519ccd277ded
Menshuu <jamshidbekuktamov1999@gmail.com>
2025-03-09 16:25:48 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,16 @@
-from aiogram import Bot, Dispatcher
-from aiogram.types import Message
-from aiogram.filters import Command
-import asyncio
-import os
+from aiogram import Bot, Dispatcher, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.dispatcher.filters import Command  # BU YERNI TO‘G‘RILANDI
+from aiogram.utils import executor
 
 TOKEN = "7923166429:AAG6JQH1xfilj8135oe-fKjC-IGNnw48RNE"
 
 bot = Bot(token=TOKEN)
-dp = Dispatcher()
+dp = Dispatcher(bot)
+
+@dp.message_handler(Command("start"))
+async def start_command(message: types.Message):
+    await message.answer("Salom! Botga xush kelibsiz!")
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
